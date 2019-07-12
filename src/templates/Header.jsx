b7bfa500ec2ee175e2fb517e9420a32390c91797
Lucas Ribeiro <luucasnck@gmail.com>
2019-07-11 23:00:59 -0300

    Last commit before Gympass hire me

--- src/templates/Header.jsx
+++ src/templates/Header.jsx
@@ -37,25 +37,25 @@ class Header extends React.Component {
     render() {
         return (
             <header>
-                <div className="container">
-                    <Navbar color="light" light expand="md">
+                <Navbar color="light" light expand="md">
+                    <div className="container">
                         <NavbarBrand href="/">Lucas Ribeiro</NavbarBrand>
                         <NavbarToggler onClick={this.toggle} />
                         <Collapse isOpen={this.state.isOpen} navbar>
                             <Nav className="ml-auto" navbar>
                                 <NavItem>
-                                    <NavLink href="https://www.linkedin.com/in/lucasnck/" target="_blank"><FontAwesomeIcon icon={faLinkedin}/> LinkedIn</NavLink>
+                                    <NavLink href="https://www.linkedin.com/in/lucasnck/" target="_blank"><FontAwesomeIcon icon={faLinkedin} /> LinkedIn</NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink href="https://github.com/lucasnck" target="_blank"><FontAwesomeIcon icon={faGithub}/> Github</NavLink>
+                                    <NavLink href="https://github.com/lucasnck" target="_blank"><FontAwesomeIcon icon={faGithub} /> Github</NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink href="https://lucasnck.github.io/" target="_blank"><FontAwesomeIcon icon={faUserCircle}/> Portfolio</NavLink>
+                                    <NavLink href="https://lucasnck.github.io/" target="_blank"><FontAwesomeIcon icon={faUserCircle} /> Portfolio</NavLink>
                                 </NavItem>
                             </Nav>
                         </Collapse>
-                    </Navbar>
-                </div>
+                    </div>
+                </Navbar>
             </header >
         )
     }

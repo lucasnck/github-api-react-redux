125eb2a8908c832d3a992b1083e36e7e85fe64f2
Lucas Ribeiro <lucas.ribeiro@multiar.com.br>
2019-07-12 13:32:53 -0300

    Updates

---
+++ precache-manifest.bcfed385aa049550ac21febd71a99f73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "163c0ac1a3abe847c3414dd03120b4bc",
+    "url": "/github-api-react-redux/index.html"
+  },
+  {
+    "revision": "738828c8e5c7ac5d40aa",
+    "url": "/github-api-react-redux/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "7107d55864551e934281",
+    "url": "/github-api-react-redux/static/css/main.50ab086f.chunk.css"
+  },
+  {
+    "revision": "738828c8e5c7ac5d40aa",
+    "url": "/github-api-react-redux/static/js/2.bf23a210.chunk.js"
+  },
+  {
+    "revision": "7107d55864551e934281",
+    "url": "/github-api-react-redux/static/js/main.e3f82b8d.chunk.js"
+  },
+  {
+    "revision": "e1e3dcfd18712882b796",
+    "url": "/github-api-react-redux/static/js/runtime~main.621eaac2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lucasnck/github-api-react-redux/precache-manifest.575a406068400c5c9759264c7f1be225.js"
+  "/github-api-react-redux/precache-manifest.bcfed385aa049550ac21febd71a99f73.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lucasnck/github-api-react-redux/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-api-react-redux/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
